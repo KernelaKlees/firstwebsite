7fbd1c4245e22f55581df033c9834ff8e31d3af2
Kernela <112568330+KernelaKlees@users.noreply.github.com>
2024-11-15 14:03:29 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal website
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Kernela/pen/rNXQeQv](https://codepen.io/Kernela/pen/rNXQeQv).
+
---
+++ src/script.js
@@ -0,0 +1,25 @@
+// Define the favouriteMovieGenre function
+function favouriteMovieGenre(genre) {
+    console.log(`Your favourite movie genre is ${genre}.`);
+}
+
+// Define the favouriteFruit function
+function favouriteFruit(fruit) {
+    console.log(`Your favourite fruit is ${fruit}.`);
+}
+
+// Define the favouriteMode function
+function favouriteMode(mode) {
+    console.log(`You prefer ${mode} mode.`);
+}
+
+// Define the favouriteEdgeStyle function
+function favouriteEdgeStyle(style) {
+    console.log(`Your preferred edge style is ${style}.`);
+}
+
+// Call the functions
+favouriteMovieGenre("fantasy");
+favouriteFruit("watermelon");
+favouriteMode("dark");
+favouriteEdgeStyle("sharp");
